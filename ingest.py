8651c8da6b0d161d99a8152eeb48306480cb550b
aburkhea <aaron.c.burkhead@gmail.com>
2024-07-07 16:31:45 -0700

    Start working on update_data

--- ingest.py
+++ ingest.py
@@ -9,24 +9,38 @@ from subprocess import Popen,PIPE
 from util import ensure_cwd
 
 
-SF_ADR = "https://api.scryfall.com/bulk-data/"
-
+# Scryfall data list
+SFDATA = [
+    "oracle-cards"
+]
+# Scryfall address
+SFADDR = 'https://api.scryfall.com/bulk-data/'
+# Get absolute parent path to __file__
 FDIR = Path(__file__).absolute().parent
 
 @ensure_cwd(ddir=FDIR)
 # Get scryfall data
 def ingest_url(url, **kw):
     # Get kws
-    odir = kw.pop("odir", None)
+    fout = kw.pop("fout", None)
     # Build command line
     cmdi = ["curl", f"{url}"]
-    if odir:
-        cmdi += ["-o", f"{odir}"]
+    if fout:
+        cmdi += ["-o", f"{fout}"]
+    # Enable stderr
+    cmdi += ["--fail", "--silent", "--show-error"]
     # Run process
-    process = Popen(cmdi, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
+    process = Popen(cmdi, stdout=PIPE, stderr=PIPE, encoding="UTF-8")
+    _, stderr = process.communicate()
+    # Raise stderr exception if exists
+    if stderr:
+        raise Exception(stderr)
 
 
-time = datetime.datetime.now()
-fout = f"oracle-cards-{time.year}-{time.month}-{time.day}"
-ingest_url(SF_ADR + "oracle-cards", odir=f"bulk_data/{fout}")
+def update_data(data_list=SFDATA, force=False):
+    time = datetime.datetime.now()
+    for data in data_list:
+        fout = f"{data}-{time.year}-{time.month}-{time.day}"
+        # Dont ingest if exists and no force, else ingest
+        if not(os.path.exists(fout)) and force:
+            ingest_url(SFADDR + f"{data}", fout=f"bulk_data/{fout}")
