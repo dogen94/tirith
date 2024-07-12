38fbc6f321dfa6df244a76d5a6506a264bac8d8c
aburkhea <aaron.c.burkhead@gmail.com>
2024-07-11 21:51:33 -0700

    Work on making db table

--- tirith/db.py
+++ tirith/db.py
@@ -1,23 +1,57 @@
 import sqlite3
+import os
 
 
-def connect_db():
+DBDIR = 'db/'
+JSONCOLS = [
+    "oracle_id",
+    "name",
+    "mana_cost",
+    "cmc",
+    "type_line",
+    "oracle_text",
+    "legalities",
+    "games",
+    "set_id",
+    "prices",
+]
+
+
+def connect_db(dbdir):
     # Connect to SQLite database (or create it if it doesn't exist)
-    conn = sqlite3.connect('db/oracle-card.db')
+    conn = sqlite3.connect(dbdir)
     # Create a cursor object using cursor() method
     cursor = conn.cursor()
-    # Create a table
-    cursor.execute('''CREATE TABLE IF NOT EXISTS cards
-                    (id CHAR(36) PRIMARY KEY, name TEXT, age INTEGER)''')
     return conn, cursor
 
+def create_table(conn, cursor, **kw):
+    exec_str = genr8_table_exec(**kw)
+    # Create a table
+    cursor.execute(exec_str)
+
+def genr8_table_exec(**kw):
+    # Get title
+    title = kw.pop("title")
+    # Start exec string
+    exec_str = f"'''CREATE TABLE IF NOT EXISTS {title} ("
+    cols = kw.pop("cols", {})
+    col_list = []
+    # Go through provided cols
+    for i,(k,v) in enumerate(cols.items()):
+        if i == 0:
+            col_list += [f"{k} {v} PRIMARY KEY"]
+        else:
+            col_list += f"{k} {v}"
+    # Assemble col string
+    col_str = ",".join(col_list)
+    exec_str += col_str + ")'''"
+    return exec_str
 
 def insert_db(conn, cursor):
     # Insert data into table
     cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Alice', 30))
     # Commit changes
     conn.commit()
-
 
 def close_db(conn, cursor):
     # Close the cursor and connection
