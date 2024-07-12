e3d965ddce96aee49bc426cab2ea61f134d76dc3
aburkhea <aaron.c.burkhead@gmail.com>
2024-07-11 22:00:06 -0700

    Move to oop

--- tirith/db.py
+++ tirith/db.py
@@ -17,44 +17,53 @@ JSONCOLS = [
 ]
 
 
-def connect_db(dbdir):
-    # Connect to SQLite database (or create it if it doesn't exist)
-    conn = sqlite3.connect(dbdir)
-    # Create a cursor object using cursor() method
-    cursor = conn.cursor()
-    return conn, cursor
+class Database(object):
+    def __init__(self, dbdir):
+        self.dbdir = dbdir
+        conn, cursor = self.connect_db(dbdir)
+        self.conn = conn
+        self.cursor = cursor
+        self.table = None
 
-def create_table(conn, cursor, **kw):
-    exec_str = genr8_table_exec(**kw)
-    # Create a table
-    cursor.execute(exec_str)
+    def connect_db(dbdir):
+        # Connect to SQLite database (or create it if it doesn't exist)
+        conn = sqlite3.connect(dbdir)
+        # Create a cursor object using cursor() method
+        cursor = conn.cursor()
+        return conn, cursor
 
-def genr8_table_exec(**kw):
-    # Get title
-    title = kw.pop("title")
-    # Start exec string
-    exec_str = f"'''CREATE TABLE IF NOT EXISTS {title} ("
-    cols = kw.pop("cols", {})
-    col_list = []
-    # Go through provided cols
-    for i,(k,v) in enumerate(cols.items()):
-        if i == 0:
-            col_list += [f"{k} {v} PRIMARY KEY"]
-        else:
-            col_list += f"{k} {v}"
-    # Assemble col string
-    col_str = ",".join(col_list)
-    exec_str += col_str + ")'''"
-    return exec_str
+    def create_table(self, conn, cursor, **kw):
+        exec_str = self.genr8_table_exec(**kw)
+        # Create a table
+        cursor.execute(exec_str)
 
-def insert_db(conn, cursor):
-    # Insert data into table
-    cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Alice', 30))
-    # Commit changes
-    conn.commit()
+    def genr8_table_exec(self, **kw):
+        # Get title
+        title = kw.pop("title")
+        self.table = title
+        # Start exec string
+        exec_str = f"'''CREATE TABLE IF NOT EXISTS {title} ("
+        cols = kw.pop("cols", {})
+        col_list = []
+        # Go through provided cols
+        for i,(k,v) in enumerate(cols.items()):
+            if i == 0:
+                col_list += [f"{k} {v} PRIMARY KEY"]
+            else:
+                col_list += f"{k} {v}"
+        # Assemble col string
+        col_str = ",".join(col_list)
+        exec_str += col_str + ")'''"
+        return exec_str
 
-def close_db(conn, cursor):
-    # Close the cursor and connection
-    cursor.close()
-    conn.close()
+    def insert2table(conn, cursor):
+        # Insert data into table
+        cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Alice', 30))
+        # Commit changes
+        conn.commit()
+
+    def close_db(conn, cursor):
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
 
