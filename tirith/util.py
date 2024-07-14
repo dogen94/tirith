96d2a52f0ec45e48a37211668be161b123c1f587
aburkhea <aaron.c.burkhead@gmail.com>
2024-07-14 13:54:48 -0700

    Add some util funcs for some db cols

--- tirith/util.py
+++ tirith/util.py
@@ -2,7 +2,35 @@
 # Standard Library
 import os
 import json
+import struct
+
 from functools import wraps
+
+
+LEGALITIES = [
+  'standard',
+  'future',
+  'historic',
+  'timeless',
+  'gladiator',
+  'pioneer',
+  'explorer',
+  'modern',
+  'legacy',
+  'pauper',
+  'vintage',
+  'penny',
+  'commander',
+  'oathbreaker',
+  'standardbrawl',
+  'brawl',
+  'alchemy',
+  'paupercommander',
+  'duel',
+  'oldschool',
+  'premodern',
+  'predh',
+]
 
 
 def ensure_cwd(*a, **kw):
@@ -34,3 +62,32 @@ def read_json(fjson):
         return jout
     else:
         raise FileExistsError(f"{fjson} does not exist")
+    
+
+def get_usd(prices):
+    v = prices.get("usd")
+    if v:
+        v = float(v)
+    return v
+
+
+def convert_legals(legalities):
+    # Probably a better way to do this
+    bitlist = []
+    for leg in LEGALITIES:
+        v = legalities[leg]
+        if v == "not_legal":
+            bitlist += [False]
+        else:
+            bitlist += [True]
+    # Pack the booleans into a byte array
+    packed_data = bytearray(struct.pack('?' * len(bitlist), *bitlist))
+    return packed_data
+
+
+def convert_blegals(blegalities):
+    num_bits = len(blegalities)
+    unpacked_data = []
+    for i in range(num_bits):
+        unpacked_data.append(struct.unpack('?', blegalities[i // 8])[0] & (1 << (i % 8)) != 0)
+    return unpacked_data
