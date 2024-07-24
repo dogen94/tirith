ecccd5945b3d72474ba55965e6f907c179ab85b4
aburkhea <aaron.c.burkhead@gmail.com>
2024-07-23 19:37:16 -0700

    Add some more messing around with profit prediction scenarios

--- minas.py
+++ minas.py
@@ -9,7 +9,7 @@ import numpy as np
 # Murders at Karlov Manor set id
 setid = "2b17794b-15c3-4796-ad6f-0887a0eceeca"
 
-def estimate_set(setid, nsim=10000):
+def estimate_booster_price(setid, nsim=50000):
     r"""wait this is gross, they keep changing all this stuff
     So a "play booster box" is 36 packs that's standard
     Then you need to estimate the value of each pack;
@@ -76,8 +76,11 @@ def estimate_set(setid, nsim=10000):
         price = 0.0
         for card in [*comdraws, *ucomdraws, *rmdraws, *wildcards]:
             if card == None:
-                card = 1.0;
-            price+= float(card) 
+                card = 0.25;
+            # Ignore anything under 0.25 as not worth selling
+            if float(card) < 0.25:
+                card = 0.0
+            price+= float(card)
         return price
     
     prices = np.empty(nsim)
@@ -85,11 +88,34 @@ def estimate_set(setid, nsim=10000):
         prices[i] = sim_pack(commons, uncommons, rares, mythics, allcards)
 
     expectation_per_booster = np.mean(prices)
-    expectation_per_bbox = expectation_per_booster * 36.0
-    return expectation_per_bbox
+    # expectation_per_bbox = expectation_per_booster * 36.0
+    return prices
 
 
-if __name__ == "__main__":
-    estimate_set(setid)
-    # _ = update_local_data(force=True)
-    # update_price_table()
+price_per_booster = estimate_booster_price(setid)
+price_per_bbox = price_per_booster*36.0
+cost_per_bbox = 101.0
+# Odds of making money buying 3 bboxes
+sample_bboxs = np.random.choice(price_per_bbox, size=(10000,3), replace=True)
+total_sample_bbox = np.sum(sample_bboxs, axis=1)
+count, bins_count = np.histogram(total_sample_bbox, bins=200) 
+# finding the PDF of the histogram using count values 
+pdf = count / sum(count) 
+# using numpy np.cumsum to calculate the CDF 
+# We can also find using the PDF values by looping and adding 
+cdf = np.cumsum(pdf)
+
+# plt.hist(total_sample_bbox, bins=50)
+# plt.show()
+I = np.where(bins_count[1:] >= cost_per_bbox*3)
+print(f"Estimated {100 *(1 - cdf[I[0][0]]):4.2f}% probability of profit buying 3 booster boxes of MKM at ${cost_per_bbox} per")
+
+# plt.plot(bins_count[1:], cdf)
+# plt.plot([cost_per_bbox*3,cost_per_bbox*3], [0,1], color="r")
+# plt.show()
+
+
+# if __name__ == "__main__":
+#     _ = update_local_data(force=True)
+#     update_price_table()
+
